e064b3b5aad5033456d4e0b8985e336715f56c3b
liyuze <ezuylee@gmail.com>
2020-04-29 16:01:47 +0800

    初始化项目

---
+++ rainze-spring-cloud-zipkin/src/main/java/com/rainze/cloud/assembly/RainzeSpringCloudEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.rainze.cloud.assembly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RainzeSpringCloudEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RainzeSpringCloudEurekaApplication.class, args);
+    }
+
+}
